73df1a792fb6abcca4ae1417b31dcdd07ba33580
Rasmus Sööt <Rasmus.Soot@nortal.com>
2019-08-23 22:58:30 +0300

    Initial commit

---
+++ src/main/java/WireMockProxyTransformer.java
@@ -0,0 +1,32 @@
+import com.github.tomakehurst.wiremock.common.FileSource;
+import com.github.tomakehurst.wiremock.extension.Parameters;
+import com.github.tomakehurst.wiremock.extension.ResponseDefinitionTransformer;
+import com.github.tomakehurst.wiremock.http.Request;
+import com.github.tomakehurst.wiremock.http.ResponseDefinition;
+import com.github.tomakehurst.wiremock.servlet.WireMockHttpServletRequestAdapter;
+
+import java.lang.reflect.Field;
+
+public class WireMockProxyTransformer extends ResponseDefinitionTransformer {
+    private static final String PREFIX_FIELD_NAME = "urlPrefixToRemove";
+
+    @Override
+    public ResponseDefinition transform(Request request, ResponseDefinition responseDefinition, FileSource files, Parameters parameters) {
+        String urlPrefixToRemove = parameters.getString(PREFIX_FIELD_NAME);
+
+        try {
+            Field field = WireMockHttpServletRequestAdapter.class.getDeclaredField(PREFIX_FIELD_NAME);
+            field.setAccessible(true);
+            field.set(request, urlPrefixToRemove);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new IllegalStateException(e);
+        }
+
+        return responseDefinition;
+    }
+
+    @Override
+    public String getName() {
+        return "my-proxy";
+    }
+}
